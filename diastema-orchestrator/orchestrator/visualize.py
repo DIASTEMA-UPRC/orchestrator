fc249486c7fefc9a0e3272367f6006398ee9469f
karamolegkos <p.karamolegos@yahoo.gr>
2023-02-23 23:49:43 +0200

    Made the visualization update.

--- diastema-orchestrator/orchestrator/visualize.py
+++ diastema-orchestrator/orchestrator/visualize.py
@@ -10,72 +10,33 @@ from Diastema_Service import Diastema_Service
 # Import Libraries
 import io
 
-def visualize(playbook, job, last_bucket):
-    '''
-    The following data must be send to the visualization service
-    {
-        "analysis-id" : <analysis-id>,
-        "job-id" : <job-id>,
-        "last-job-id" : <last-job-id>,
-        "last-job-title" : <last-job-title>,
-        "minio-output" : <minio-output>,
-        "minio-input" : <minio-input>
-    }
-    '''
+DIASTEMA_VISUALIZATION_BUCKET = "diastemaviz"
 
-    # The Data to be visualised are saved in the bucket below
-    analysis_id = playbook["analysis-id"]
-    job_id = job["id"]
-    last_job_id = job["from"]
-    last_job_title = "" # To be found
-    minio_output = normalised(playbook["database-id"]) + "/analysis-" + normalised(playbook["analysis-id"]) + "/visualization-" + normalised(job["step"])
-    minio_input = last_bucket
+def visualize(playbook, job, last_bucket):
+    last_job_from_step = job["from"]
 
     # Use all the playbook to find the last job
     jobs = playbook["jobs"]
 
     for i_job in jobs:
-        if(i_job["id"] == last_job_id):
-            last_job_title = i_job["title"]
+        if(i_job["step"] == last_job_from_step):
+            last_job_id = i_job["id"]
             break
-    
-    # Make the MinIO Analysis buckers
+
+    # Make the MinIO object
     minio_obj = MinIO_Class()
-    minio_obj.put_object(normalised(playbook["database-id"]), "analysis-"+normalised(playbook["analysis-id"])+"/visualization-"+normalised(job["step"])+"/", io.BytesIO(b""), 0,)
-
-    form_data = {
-        "analysis-id":playbook["analysis-id"],
-        "job-id" : job_id,
-        "last-job-id" : last_job_id,
-        "last-job-title" : last_job_title,
-        "minio-output" : minio_output,
-        "minio-input" : minio_input
-    }
-
-    # Start Loading Service
-    service_obj = Diastema_Service()
-    service_obj.startService("visualization", form_data)
-
-    # Wait for loading to End
-    job_res = service_obj.waitForService("visualization", job["id"])
-
-    if job_res["status"] == "error":
-        # Contact front end for the error of the job
-        front_obj = FrontEnd_Class()
-        front_obj.diastema_call(message = "error", update = job_res["message"]+" for the analysis with ID: "+playbook["analysis-id"])
-        return minio_output, True
 
     # Insert the visualization data in MongoDB
-    job_record = {"minio-path": minio_output, "directory-kind":"visualization", "job-json":job}
+    job_record = {"minio-path": DIASTEMA_VISUALIZATION_BUCKET+"/"+str(last_job_id), "directory-kind":"visualization", "job-json":job}
 
     mongo_obj = MongoDB_Class()
     mongo_obj.insertMongoRecord(normalised(playbook["database-id"]), "analysis_"+normalised(playbook["analysis-id"]), job_record)
 
     # Get object names from MinIO Storage
-    graph_names = minio_obj.getObjectsOfPath(normalised(playbook["database-id"]), "analysis-"+normalised(playbook["analysis-id"])+"/visualization-"+normalised(job["step"])+"/")
+    graph_names = minio_obj.getObjectsOfPath(DIASTEMA_VISUALIZATION_BUCKET, str(last_job_id)+"/")
     path_names = []
     for graph_name in graph_names:
-        path_names.append(minio_output+"/"+graph_name)
+        path_names.append(DIASTEMA_VISUALIZATION_BUCKET+"/"+str(last_job_id)+"/"+graph_name)
 
     # Update Mongo Web Application metadata
     mongo_obj.updateMongoVisualization("UIDB", "pipelines", { "analysisid" :  playbook["analysis-id"]}, {"label": job["label"], "value": path_names})
